701a147d449575c08dedfd17b99a430e00698c2c
nick30441 <nicholas.novak@okstate.edu>
2024-05-05 22:52:57 -0500

    done

--- SV/lfsr.sv
+++ SV/lfsr.sv
@@ -2,7 +2,7 @@ module lfsr(input logic [15:0] seed, input logic clk, reset, output logic [15:0]
 
 always_ff @(posedge clk or posedge reset) begin
     if (reset) shift_seed = seed;
-    else shift_seed = {shift_seed[14:0], shift_seed[15] ^ shift_seed[14] ^ shift_seed[12] ^ shift_seed[3]};
+    else shift_seed = {shift_seed[14:0], shift_seed[15] ~^ shift_seed[14] ~^ shift_seed[12] ~^ shift_seed[3]};
 end
 
 endmodule
@@ -10,7 +10,7 @@ endmodule
 module lfsr64 (input logic [63:0] seed, input logic clk, lfsr_go, output logic [63:0] shift_seed);
 
 always_ff @(posedge clk /*or posedge lfsr_go*/) begin
-    if(lfsr_go) shift_seed = {shift_seed[62:0], shift_seed[63] ^ shift_seed[62] ^ shift_seed[60] ^ shift_seed[59]};
+    if(lfsr_go) shift_seed = {shift_seed[62:0], shift_seed[63] ~^ shift_seed[62] ~^ shift_seed[60] ~^ shift_seed[59]};
     else shift_seed = seed;
 end
  
